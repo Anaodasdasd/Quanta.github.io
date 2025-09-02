e32c7cd11f8454759ad3ec0e0c870d941ba7e775
Ares <tomislavvrancik@gmail.com>
2025-09-02 22:40:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function updateNavigation() {
         if (history.length === 0) {
-            navPath.textContent = "Main";
+            navPath.textContent = "Menu";
             navCurrent.textContent = " › Main";
             return;
         }
